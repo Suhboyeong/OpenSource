149395743f3b51aa46126d832a2bd1f2c87892a6
Suhboyeong <113081606+Suhboyeong@users.noreply.github.com>
2023-12-13 23:41:25 +0900

    Add files via upload

---
+++ opencv_1.cpp
@@ -0,0 +1,13 @@
+//헤더 파일 포함하여 사용하기 위한 선언
+#include "Photoshop.h"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	//Photoshop 클래스의 인스턴스를 생성하여 프로그램을 시작합니다.
+	PhotoShop pt;
+	waitKey(0);
+	return 0;
+
+}
